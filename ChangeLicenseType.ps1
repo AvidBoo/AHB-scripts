a7dcc489fbef7b48c462f11606d74930d7b6aac7
David Booth <51790502+AvidBoo@users.noreply.github.com>
2022-03-17 16:09:18 -0600

    Add files via upload

---
+++ ChangeLicenseType.ps1
@@ -0,0 +1,54 @@
+﻿Set-StrictMode -Version 1.0
+#
+#Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
+# Comment out if module not imported
+Import-Module Az
+
+if ([string]::IsNullOrEmpty($(Get-AzContext).Account)){(Login-AzAccount)}
+
+#Enter $null if you want all subscriptions scanned.   
+$SubscriptionId = $null 
+$Subscriptions = Get-Azsubscription
+
+if ($SubscriptionId)
+{
+    $Subscriptions = $Subscriptions | Where-Object { $_.SubscriptionId -EQ $SubscriptionId }
+}
+
+$i=0
+
+foreach ( $Subscription in $Subscriptions ) {
+
+    $SubscriptionId = $Subscription.SubscriptionId
+
+     (Select-AzSubscription -SubscriptionId $SubscriptionId)>0
+
+    # Display progress, this script may take a while
+    $i++
+    Write-Progress -activity $subscription.Name -PercentComplete ($i/$Subscriptions.Count*100)
+
+    # Get all of the VM's:
+    # ($vms=Get-AzVM) > 0
+    $vms = Get-AzVM | Where-Object{$_.LicenseType -notlike "Windows_Server" -and $_.StorageProfile.OsDisk.OsType -like "Windows" } 
+    $vmNum = 0
+    $vmCount = $vms.Count
+
+    # Add info about VM's from the Resource Manager to the array
+    foreach ($vm in $vms)
+    {    
+        $vmNum++
+        Write-Progress -activity $vm.Name -PercentComplete ($vmNum/$vmCount*100)
+
+        try{
+            $vm = Get-AzVM -ResourceGroup  $vm.ResourceGroupName -Name $vm.Name
+            $vm.LicenseType = "Windows_Server"  # Use None for PAYGO i.e., NO AHB. Use Windows_Server for AHB
+            Update-AzVM -ResourceGroupName $vm.ResourceGroupName -VM $vm
+
+    }
+    catch{
+        Write-Output "Problem VM Name: $($vm.Name) in $($Subscription.Name) - Error Message: [$($_.Exception.Message)]"
+    }
+
+       
+    }
+}
---
+++ ListAllVMsInSubscription.ps1
@@ -0,0 +1,78 @@
+﻿Set-StrictMode -Version 1.0
+#
+#Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
+# UnComment out if module not imported
+Import-Module Az
+
+if ([string]::IsNullOrEmpty($(Get-AzContext).Account)){(Login-AzAccount)}
+
+#Enter $null if you want all subscriptions scanned or per subscription with "subscription guid"
+#$SubscriptionId = "subsription GUID"
+$SubscriptionId = $null  
+$Subscriptions = Get-Azsubscription
+# $home/ puts the report in the root of your personal directory
+$ReportName = "AHBUsed.csv" 
+
+if ($SubscriptionId)
+{
+    $Subscriptions = $Subscriptions | Where-Object { $_.SubscriptionId -EQ $SubscriptionId }
+}
+
+$vmarray = @()
+$i=0
+$vmstatus = $null
+
+foreach ( $Subscription in $Subscriptions ) {
+
+    $SubscriptionId = $Subscription.SubscriptionId
+
+     (Select-AzSubscription -SubscriptionId $SubscriptionId)>0
+
+    # Display progress, this script may take a while
+    $i++
+    Write-Progress -activity $subscription.Name -PercentComplete ($i/$Subscriptions.Count*100)
+
+    # Get all of the VM's:
+    ($vms=Get-AzVM) > 0
+    $vmNum = 0
+    $vmCount = $vms.Count
+
+    # Add info about VM's from the Resource Manager to the array
+    foreach ($vm in $vms)
+    {    
+        $vmNum++
+        Write-Progress -activity $vm.Name -PercentComplete ($vmNum/$vmCount*100)
+
+        try{
+            $vmstatus = Get-AzVM -Name $vm.Name -ResourceGroupName $vm.ResourceGroupName -Status 
+            # Add values to the array: 
+            $vmarray += New-Object PSObject -Property @{
+                Subscription=$Subscription.Name; 
+                ResourceGroupName = $vm.ResourceGroupName; 
+                Location = $vm.Location;
+                Name=$vm.Name; 
+                PowerState=(get-culture).TextInfo.ToTitleCase(($vmstatus.statuses)[1].code.split("/")[1]); 
+                OSType = $vm.StorageProfile.OsDisk.OsType; 
+                OSName = $vmstatus.OsName;
+                OSVersion = $vmstatus.OsVersion;
+                ImageReference=$vm.StorageProfile.ImageReference.Offer + " $($vm.StorageProfile.ImageReference.Sku)"; 
+                Size=$vm.HardwareProfile.VmSize; 
+                NumberOfCores = (Get-AzVMSize -location $vm.Location | Where-Object { $_.name -eq $vm.HardwareProfile.VmSize }).NumberOfCores; 
+                MemoryInMB = (Get-AzVMSize -location $vm.Location | Where-Object { $_.name -eq $vm.HardwareProfile.VmSize }).MemoryInMB ; 
+                AhbWsByol = $vm.LicenseType;
+                SQLLicenseType = (Get-AzSqlVM -Name $vm.Name -ResourceGroupName $vm.ResourceGroupName -ErrorAction SilentlyContinue).LicenseType ; 
+                SQLEdition = (Get-AzSqlVM -Name $vm.Name -ResourceGroupName $vm.ResourceGroupName -ErrorAction SilentlyContinue).Sku ; 
+                SQLOffer = (Get-AzSqlVM -Name $vm.Name -ResourceGroupName $vm.ResourceGroupName -ErrorAction SilentlyContinue).Offer ; 
+                SQLManagementType = (Get-AzSqlVM -Name $vm.Name -ResourceGroupName $vm.ResourceGroupName -ErrorAction SilentlyContinue).SqlManagementType ; 
+             } 
+
+    }catch{
+        Write-Output "Problem VM Name: $($vm.Name) in $($Subscription.Name) - Error Message: [$($_.Exception.Message)]"
+    }
+
+       
+    }
+}
+
+$vmarray | Select-Object Subscription, ResourceGroupName,Location, Name,PowerState,OSType,OSName,OSVersion,
+ImageReference,Size,NumberOfCores,MemoryInMB,AhbWsByol, SQLLicenseType,SQLEdition,SQLOffer,SqlManagementType | Export-Csv -NoTypeInformation -Path $ReportName
